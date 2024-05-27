31227d630c2b982a00d0f1416fa8a94e8bb77e36
zmart64 <marteneckardt@hotmail.com>
2024-05-27 16:46:49 +0200

    allowed newer np-versions

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     test_suite="tests",
     python_requires='>=3.7',
     install_requires=[
-        "numpy<1.22.0",
+        "numpy>=1.22.0",
         "scikit-learn>=0.15.0,<1.1.0",
         "scipy>=0.11,<1.8.0",
         "tensorflow>=2.0.0",
